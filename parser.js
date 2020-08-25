b646ab19648daf9a24426c42156d79b8f8530961
jstiff <jwstiff8@gmail.com>
2020-08-25 12:12:09 -0500

    update

--- parser.js
+++ parser.js
@@ -5,17 +5,25 @@ const path = require("path");
 
 const csvFile = path.join(__dirname, "MOCK_DATA.csv");
 const titlesTxt = path.join(__dirname, "./titles.txt");
+const umnFile = path.join(__dirname, "UMN_DATA.csv");
 
 fs.createReadStream(csvFile)
   .pipe(csv([]))
   .on("data", (data) => results.push(data))
   .on("end", () => {
     console.log(results.length);
-    let poop = results.forEach((obj) => {
-      console.log(obj[3]);
+    // let poop = results.forEach((obj) => {
+    //   console.log(obj[3]);
+    // });
+    let poop2 = results.map((obj) => {
+      return {
+        name: obj[1],
+        email: obj[3],
+      };
     });
-    console.log(poop);
-    fs.appendFile(titlesTxt, poop, (err) => {
+    console.log(poop2);
+    //console.log(poop);
+    fs.appendFile(titlesTxt, poop2, (err) => {
       if (err) throw err;
       console.log('The "data to append" was appended to file!');
     });
